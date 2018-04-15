40cb11e66d1995114e2a2c02de920367541e0e37
lupaz <eslupaz@hotmail.com>
2018-04-14 20:23:48 -0600

    Commit inicial del proyecto

---
+++ config/services.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'mandrill' => [
+        'secret' => env('MANDRILL_SECRET'),
+    ],
+
+    'ses' => [
+        'key'    => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'stripe' => [
+        'model'  => App\User::class,
+        'key'    => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+
+    'facebook' => [
+        'client_id' => '129356554446515',
+        'client_secret' => '7e86913dde3bf1c3e1b91ab1b60fc74e',
+        'redirect' => 'http://ohmylamp.com/auth/facebook/callback'
+    ]
+
+];
