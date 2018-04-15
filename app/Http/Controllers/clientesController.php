bff28de82e32fa62e9a8e6b0f6d24aedf2aa4d8e
lupaz <eslupaz@hotmail.com>
2018-04-15 12:19:29 -0600

    Se agrego la funcion Store en el cotrolador cliente

--- app/Http/Controllers/clientesController.php
+++ app/Http/Controllers/clientesController.php
@@ -58,4 +58,44 @@ class clientesController extends Controller
 
     }
 
+    public function store(Request $request)
+    {
+
+        //dd($request->all());
+        /*if(!preg_match("/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*\W)(?=.*[A-Z]).*$/", $request->pass)){
+            flash('ERROR: la contraseña debe tener almenos: 8 caracteres, un numero, un simbolo y una letra mayuscula.')->error()->important();
+            return redirect()->back()->withInput();
+        }*/
+
+        //validacion de uuario repetido
+
+        $user = DB::table('usuario')->where('user','=',$request->user)->value('user');
+
+        if(!empty($user)){
+            flash('ERROR: El usuario ingresado ya se encuentra registrado.')->error()->important();
+            return redirect()->back()->withInput();  
+            echo $email;
+        }
+
+        if($request->pass != $request->pass2){
+            flash('ERROR: Las contraseñas no coinciden, intente de nuevo.')->error()->important();
+            return redirect()->back()->withInput();
+        }
+
+        $codigo = rand(10000, 99999);
+
+        DB::insert('INSERT INTO usuario (nombre,correo,user,pass,codigo) values (?,?,?,?,?)', [$request->name,$request->correo,$request->user,$request->pass,$codigo]);
+
+        $cod_user = DB::table('usuario')->where('user','=',$request->user)->value('cod_user');
+
+        //creamos la cuenta del usuario
+        $num_cuenta = rand(1000000000, 9999999999);
+        $saldo = 0;
+
+        DB::insert('INSERT INTO cuenta (num_cuenta,saldo,cod_user) values (?,?,?)', [$num_cuenta,$saldo,$cod_user]);
+
+        flash('REGISTRO EXITOSO: Le hemos enviado un correo con el codigo de acceso. #Codigo: '.$codigo)->success()->important();
+        return redirect()->back();  
+    }
+
 }
